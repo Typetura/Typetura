2bb10b494f2ad6aaa564fe1162d68c0576dde93a
Scott Kellum <scott@scottkellum.com>
2019-07-17 21:17:22 -0400

    Target root instead of body

--- js/typetura.js
+++ js/typetura.js
@@ -1,7 +1,7 @@
 function typeturaInit(global, el) {
   // Query the width of the typetura container
   function typetura() {
-    document.body.style.setProperty('--tt-width', global.offsetWidth);
+    document.documentElement.style.setProperty('--tt-width', global.offsetWidth);
     el.forEach(element => {
       element.style.setProperty('--tt-bind', element.offsetWidth);
     });
@@ -15,14 +15,14 @@ function typeturaInit(global, el) {
   var stylesheet = document.createElement('style');
   // Typetura's custom properties
   stylesheet.innerHTML =
-    'body{--tt-ease:linear;--tt-max:1600;--tt-bind:var(--tt-width);}*{--tt-key:none;animation:var(--tt-key) 1s var(--tt-ease) 1 calc(-1s*var(--tt-bind)/var(--tt-max)) paused}';
+    ':root{--tt-ease:linear;--tt-max:1600;--tt-bind:var(--tt-width);}*,:before,:after,:root{--tt-key:none;animation:var(--tt-key) 1s var(--tt-ease) 1 calc(-1s*var(--tt-bind)/var(--tt-max)) paused}';
   // Write typetura proprties to the top of the document head to avoid cascade conflicts
   document.head.insertBefore(stylesheet, document.head.firstChild);
 }
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    var global = document.getElementById('typetura') || document.body;
+    var global = document.getElementById('typetura') || document.documentElement;
     var el = document.querySelectorAll('.typetura');
 
     // Initialize width variable
